89749359c27b51213fdafcd3ee5764febabd00d0
don-satur <alanoroscomp@gmail.com>
2026-09-24 14:55:13 -0300

    entrega final del proyecto

--- analisis_smn.py
+++ analisis_smn.py
@@ -1,6 +1,6 @@
 from funciones_smn import leer_observaciones, mostrar_resumen
 
-if __name__ == "__main__":
+def main():
     archivo_1 = "datos/observaciones_smn.txt" #archivo_1 son los datos del primer archivo de datos
     archivo_2 = "datos/observaciones2_smn.txt" #archivos_2 son los datos del segundo archivo de datos
 
@@ -13,3 +13,6 @@ if __name__ == "__main__":
     print(" >>> segunda semana <<< ")
     datos_s2 = leer_observaciones(archivo_2) #resumen del segundo archivo
     mostrar_resumen(datos_s2)
+
+if __name__ == "__main__":
+    main
